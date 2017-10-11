2820f20f21040113e5edd898bb393441e52498b4
gaochao <54423923@qq.com>
2017-10-11 17:53:27 +0800

    d

---
+++ website1/inc/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>website1</title>
+</head>
+<body>
+	<ul>
+		<li><a href="index.php">home</a></li>
+		<li><a href="about.php">about</a></li>
+		<li><a href="contact.php">contact</a></li>
+	</ul>
---
+++ website1/index.php
@@ -0,0 +1,14 @@
+<?php include"./inc/header.php" ?>
+<?php include_once"./inc/header.php" ?>
+<h1>Home</h1>
+<?php include "./inc/footer.php" ?>
+
+
+<!-- include require 
+	如果有报错信息 include会显示报错信息,但不影响后面代码的执行,
+	如果有报错信息,require会显示报错信息,后面的代码不会执行,
+	include_once require_once的作用
+	重复引入 只识别一次!
+
+
+-->
